a87adad04f85b42873ced8509d9a70652629f944
juan sebastian martin <juanitomartin15006@gmail.com>
2026-03-11 08:47:42 -0500

    actualizacion de 4

--- 4.js
+++ 4.js
@@ -1,1 +1,4 @@
 console.log("git para commit en un branch B")
+console.log("git para commit en un branch A")
+console.log("git para commit en un branch A")
+console.log("git para commit en un branch A")
